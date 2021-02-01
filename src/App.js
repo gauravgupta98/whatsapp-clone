65938e0024ae6647012204c6ce80a139412abb18
Gaurav Gupta <g1561998@gmail.com>
2021-02-01 13:53:49 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Whatsapp Clone</h1>
+      <div className="app__body"></div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCMxQ0uQdrwlS3F62fPWG8PVXl5jTOyau8",
+  authDomain: "whatsapp-clone-36b61.firebaseapp.com",
+  projectId: "whatsapp-clone-36b61",
+  storageBucket: "whatsapp-clone-36b61.appspot.com",
+  messagingSenderId: "301295243545",
+  appId: "1:301295243545:web:124c4b8f432de985ddf588",
+  measurementId: "G-MQHRE803VB",
+};
