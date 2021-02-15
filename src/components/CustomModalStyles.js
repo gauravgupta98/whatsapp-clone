b8c7f1e687df6b26f17844119ba150ac812d7846
Gaurav Gupta <g1561998@gmail.com>
2021-02-15 11:16:16 +0530

    redesigned add new chat functionality

---
+++ src/components/CustomModalStyles.js
@@ -0,0 +1,18 @@
+export const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    transform: "translate(-50%, -50%)",
+    background: "#f7f7f7",
+    borderRadius: "15px",
+    borderColor: "white",
+    maxWidth: "28em",
+    minWidth: "28em",
+    minHeight: "9em",
+  },
+  overlay: {
+    background: "#9e9e9e69",
+  },
+};
--- src/components/SidebarChat/SidebarChat.js
+++ src/components/SidebarChat/SidebarChat.js
@@ -1,13 +1,19 @@
+/* eslint-disable no-undef */
 import React, { useEffect, useState } from "react";
 import { Avatar } from "@material-ui/core";
 import { Link } from "react-router-dom";
+import Modal from "react-modal";
 
 import db from "../../firebase";
 import "./SidebarChat.css";
+import { customStyles } from "../CustomModalStyles";
 
 function SidebarChat({ id, name, addNewChat }) {
   const [seed, setSeed] = useState("");
   const [messages, setMessages] = useState("");
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [roomName, setRoomName] = useState("");
+
   useEffect(() => {
     if (id) {
       db.collection("rooms")
@@ -24,19 +30,41 @@ function SidebarChat({ id, name, addNewChat }) {
     setSeed(Math.floor(Math.random() * 5000));
   }, []);
 
-  const createChat = () => {
-    const roomName = prompt("Please enter name for chat");
+  const openModal = () => setModalIsOpen(true);
+  const closeModal = () => setModalIsOpen(false);
 
-    if (roomName) {
+  const createChat = (e) => {
+    e.preventDefault();
+
+    if (roomName && roomName !== "") {
       db.collection("rooms").add({
         name: roomName,
       });
+      setRoomName("");
+      setModalIsOpen(false);
     }
   };
 
   return addNewChat ? (
-    <div onClick={createChat} className="sidebarChat">
-      <h2>Add New Chat</h2>
+    <div className="newChat__Modal">
+      <div onClick={openModal} className="sidebarChat">
+        <h2>Add New Chat</h2>
+      </div>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Modal"
+        style={customStyles}
+      >
+        <h1>Create new chat</h1>
+        <form onSubmit={(e) => createChat(e)}>
+          <input
+            placeholder="Room name"
+            onChange={(e) => setRoomName(e.target.value)}
+          />
+          <button type="submit">Create</button>
+        </form>
+      </Modal>
     </div>
   ) : (
     <Link to={`/rooms/${id}`}>
